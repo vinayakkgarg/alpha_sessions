2c86d0713472552ea3941697e1ffee3b49dee9e6
vinayakkgarg <vinayakk.garg99@gmail.com>
2018-08-26 01:30:00 +0530

    fixed and removed files

--- session_1/helloworld.py
+++ session_1/helloworld.py
@@ -1,8 +1,10 @@
 print("helloworld")
 print('hello world')
+#The above 2 lines behave in exactly the same manner
+
 print(''' hello			
 			
 			
 		world''')
 
-
+#The third line preserves the formatting styles for the output
--- session_1/sum.py
+++ session_1/sum.py
@@ -1,4 +1,6 @@
 a = 3
 b = 4
-a + b
+#a and b are implicitly assigned to be integer variables
 
+print(a + b)
+#inverted commas are not used to print out variables
