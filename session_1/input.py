9324a5e69bb284b3194af84d88441aa2d2a310a4
vinayakkgarg <vinayakk.garg99@gmail.com>
2018-08-26 01:57:24 +0530

    added all code files

---
+++ session_1/input.py
@@ -0,0 +1,9 @@
+#This is how you take an input from the user.
+
+a = int(input())
+b = int(input())
+c = a+b
+print(c)
+
+#Explicit definition of int is done becuase by default input function returns a string.
+#A plus operation just concatenates the string.
--- session_1/project1.py
+++ session_1/project1.py
@@ -1,6 +1,17 @@
-x = int(input())	
-y = int(input())	
-z = x+y
-print(z)
+#This is the first simple project
+#Take user name, age, batch as input
+#Display the three outputs in a single line as:
+# <username> is <age> years old and is in batch of <batch> .
 
-# rctvybuhnimokpl
+print("Enter Name:")
+name = input()
+print("Enter Age:")
+age = int(input())
+print("Enter Batch Year:")
+batch = int(input())
+print(name + " is " + str(age) + " years old and is in batch of " + str(batch))
+
+#Explicit definition of str is done for age and batch as they are integer variables
+#Addition of an integer to a string doesnt make sense
+#name variable however can be simply added to the rest of the string as it takes in standard input which is a string
+
